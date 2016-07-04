de269e162965bd9632039bb7828f11bab622d735
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-07-04 15:43:07 +0200

    --no-sandbox config

--- karma.conf.js
+++ karma.conf.js
@@ -89,7 +89,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Chrome_Travis_CI'] : ['Chrome'],
 
     customLaunchers: {
       Chrome_Travis_CI: {
