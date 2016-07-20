cb3e47e5c03d8c3396a7b8b47a7252db7648707c
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-07-20 13:33:11 +0200

    added index.js as jsnext:main file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { withLayers, withoutLayers, layer, proceed, Layer } from "./contextjs.js";
-import { notify } from './copv2/activeEventTracking.js';
+import notify from './copv2/activeEventTracking.js';
 
 notify('click', 'div', () => { console.log('index.js of composite-scope-all')})
     .uninstall();
+
+import foo from 'stack-es2015-modules';
+
+export { foo as Stack };
