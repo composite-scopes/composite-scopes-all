4804213ddce89fe5bc923600b2623654b001d003
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-07-29 14:21:09 +0200

    added tests for system wide object redefinition

--- src/mixin.js
+++ src/mixin.js
@@ -1,3 +1,54 @@
 // TODO: an adapter to ContextJS 2.0
-export * as cop from "contextjs";
+import { Partial } from './copv2/scope.js';
+import * as cop from "contextjs";
 
+export {proceed} from "contextjs";
+
+export class Mixin extends Partial {
+    constructor() {
+        super();
+
+        this._layer = new cop.Layer();
+    }
+
+    refineClass(...args) {
+        this._layer.refineClass(...args);
+
+        return this;
+    }
+    refineObject(...args) {
+        this._layer.refineObject(...args);
+
+        return this;
+    }
+    refineActivatedInstances() {
+        throw new Error('not yet implemented');
+    }
+
+    __activate__() {
+        super.__activate__();
+
+        this._layer.beGlobal();
+    }
+    __deactivate__() {
+        super.__deactivate__();
+
+        this._layer.beNotGlobal();
+    }
+    __activateFor__(obj) {
+        super.__activateFor__(obj);
+    }
+    __deactivateFor__(obj) {
+        super.__deactivateFor__(obj);
+    }
+}
+
+export default function mixin(...args) {
+    return new Mixin(...args);
+}
+
+// proceed alternative
+//import {proceed as copProceed} from "contextjs";
+// export function proceed(...args) {
+//     return copProceed(...args);
+// }
--- test/mixin-adapter.spec.js
+++ test/mixin-adapter.spec.js
@@ -1,54 +1,57 @@
 'use strict';
 
-import mixin from '../src/mixin.js';
-import {layer, proceed, withLayers} from 'contextjs';
+import mixin, { proceed } from '../src/mixin.js';
 
 describe('Mixin Adapter', () => {
 
-    describe('ContextJS', () => {
+    it('basic adapter', () => {
+        let obj = {
+            value: 17,
+            getValue: function() {
+                return this.value;
+            }
+        };
 
-        it('beGlobal', () => {
-            let obj = {
-                value: 17,
-                getValue: function() {
-                    return this.value;
-                }
-            };
-
-            var l = layer('foo')
-                .refineObject(obj, {
-                    getValue: function() {
-                        return 42 + proceed();
-                    }
-                });
-
-            expect(obj.getValue()).to.equal(17);
-
-            l.beGlobal();
-
-            expect(obj.getValue()).to.equal(42 + 17);
+        let dynamicMixin = mixin().refineObject(obj, {
+            getValue: function() {
+                return 42 + proceed();
+            }
         });
 
-        it('instance-specific layer activation', () => {
-            let obj = {
-                value: 17,
-                getValue: function() {
-                    return this.value;
-                }
-            };
+        expect(obj.getValue()).to.equal(17);
 
-            var l = layer('foo')
-                .refineObject(obj, {
-                    getValue: function() {
-                        return 42 + proceed();
-                    }
-                });
+        dynamicMixin.activate();
 
-            expect(obj.getValue()).to.equal(17);
+        expect(obj.getValue()).to.equal(42 + 17);
 
-            l.beGlobal();
+        dynamicMixin.deactivate();
 
-            expect(obj.getValue()).to.equal(42 + 17);
-        });
+        expect(obj.getValue()).to.equal(17);
     });
+
+    it('overlapping mixins', () => {
+        let obj1 = { getValue: () => 1 },
+            obj2 = { getValue: () => 2 },
+            obj3 = { getValue: () => 3 },
+            mixin1 = mixin()
+                .refineObject(obj1, { getValue: function() { return 10 + proceed(); }})
+                .refineObject(obj2, { getValue: function() { return 10 + proceed(); }})
+                .activate(),
+            mixin2 = mixin()
+                .refineObject(obj2, { getValue: function() { return 20 + proceed(); }})
+                .refineObject(obj3, { getValue: function() { return 20 + proceed(); }})
+                .activate();
+
+        expect(obj1.getValue()).to.equal(1 + 10);
+        expect(obj2.getValue()).to.equal(2 + 10 + 20);
+        expect(obj3.getValue()).to.equal(3      + 20);
+
+        mixin2.deactivate();
+
+        expect(obj1.getValue()).to.equal(1 + 10);
+        expect(obj2.getValue()).to.equal(2 + 10);
+        expect(obj3.getValue()).to.equal(3);
+    });
+
+    xit('instance-specific layer activation', () => {});
 });
